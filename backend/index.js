cb38ebe8bf0776ebd95322ca56df3c21eb6eb2b3
Mouhssine Kinani <kinanimouhssine@gmail.com>
2025-06-28 01:31:57 +0100

    add the cors in the backend\index.js and implement the ui and logic signUp and verification email in the front end

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,16 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 // auth
 import authRoutes from "./routes/auth.route.js";
-// import cors from "cors";
+import cors from "cors";
 
 
 const app = express();
 //the port
 const PORT = process.env.BACK_END_PORT ||5000;
-// app.use(cors({
-//   origin: "http://localhost:3000", // Replace with your frontend URL
-//   credentials: true
-// }));
-// app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Replace with your frontend URL
+  credentials: true
+}));
 // to be able to deal with json
 app.use(express.json());
 app.use(cookieParser())
---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const API_AUTH_URL = "http://localhost:5001/api/auth";
+axios.defaults.withCredentials = true;
+export const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
+
+  signup: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_AUTH_URL}/signup`, {
+        email,
+        password,
+        name,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response?.data?.message || "Signup failed",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+  verifyEmail: async (verificationCode) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_AUTH_URL}/verify-email`, {
+        verificationToken: verificationCode,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+        error: null,
+      });
+      return response.data;
+    } catch (error) {
+      set({
+        error: error.response?.data?.message || "Email verification failed",
+      });
+      throw error;
+    }
+  },
+}));
